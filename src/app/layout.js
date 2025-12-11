745206db1e93c3bc6045853c1985856b2017734f
Damilola91 <damilola.wiwoloku@gmail.com>
2025-12-11 10:17:25 +0100

    merged refactor with main

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
-import { getSession } from "@/lib/session";
-import Navbar from "@/components/Navbar/Navbar";
+
 import { ReduxProvider } from "@/providers/ReduxProvider";
 
 export default async function RootLayout({ children }) {
