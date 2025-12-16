6845236487093570403bd70f30754cf7e5da50ff
Damilola91 <damilola.wiwoloku@gmail.com>
2025-12-16 11:02:54 +0100

    feat: add Suspense in login success

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,7 +1,18 @@
+import { Suspense } from "react";
 import SuccessLoginGoogle from "@/components/SuccessLoginGoogle/SuccessLoginGoogle";
 
 const LoginSuccess = () => {
-  return <SuccessLoginGoogle />;
+  return (
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
+      <SuccessLoginGoogle />
+    </Suspense>
+  );
 };
 
 export default LoginSuccess;
